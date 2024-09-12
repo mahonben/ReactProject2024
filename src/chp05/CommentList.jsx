dd2c777bfb42a92a4d5b939086f828e23074572e
mahonben <mhr3429@gmail.com>
2024-09-12 16:00:29 +0900

    React 5장 실습

---
+++ src/chp05/CommentList.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import Comment from "./Comment";
+
+const comments = [
+    {
+        name: "마현빈",
+        comment: "집 가고 싶다",
+    },
+    {
+        name: "마빈",
+        comment: "집가고싶다",
+    },
+    {
+        name: "현빈",
+        comment: "집 가 고 싶 다",
+    },
+];
+
+function CommentList(props) {
+    return (
+        <div>
+            {comments.map((comment) => {
+                return (
+                    <Comment name={comment.name}
+                    comment={comment.comment} />
+                );
+            })}
+        </div>
+    );
+}
+
+export default CommentList;
--- src/index.js
+++ src/index.js
@@ -1,21 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import './chp03/Book.css'
-import './chp04/Button.css'
-import './chp04/Clack.css'
+// import './chp03/Book.css'
+// import './chp04/Button.css'
+//import './chp04/Clack.css'
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Library from "./chp03/Library";
 import Button from "./chp04/Button";
 import ConfirmDialog from "./chp04/ConfirmDialog";
 import Clock from "./chp04/Clock";
+import CommentList from "./chp05/CommentList";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 setInterval(()=>{
         root.render(
             <React.StrictMode>
-                <Clock />
+                <CommentList />
             </React.StrictMode>
         );
     }
