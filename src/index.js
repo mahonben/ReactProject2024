ebdd3448f6ee88381fdc8eddd4635ba712e6e522
mahonben <mhr3429@gmail.com>
2024-11-21 16:27:36 +0900

    React 11장 폼(제어 컴포넌트, textarea태그, select태그, File input태그, 여러입력 다루기)

--- src/index.js
+++ src/index.js
@@ -33,12 +33,17 @@ import AppNumberList from "./chp10/AppNumberList";
 import StudentAttendance from "./chp10/StudentAttendance";
 import AppStudentAttendance from "./chp10/AppStudentAttendance";
 import AttendanceCard from "./chp10/avatarAttendance/AttendanceCard";
+import NameForm from "./chp11/NameForm";
+import RequestForm from "./chp11/RequestForm";
+import NameRequestForm from "./chp11/NameRequestForm";
+import SignUp from "./chp11/SignUp";
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
         root.render(
             <React.StrictMode>
-                <AttendanceCard/>
+                <SignUp/>
             </React.StrictMode>
         );
 // If you want to start measuring performance in your app, pass a function
